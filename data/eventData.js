9cf1b90569fa1abf77c369b2fc6cd7a7d9eeed22
kamimochi <98996577+kamimochi@users.noreply.github.com>
2025-04-25 11:04:38 +0900

    Create eventData.js

---
+++ data/eventData.js
@@ -0,0 +1,127 @@
+export const eventList = [
+  {
+    name: "ハイランダー鉄道爆走事件〜そして列車はなくなった〜",
+    short: "HTE",
+    bgm: "ev_bgm_01"
+  },
+  {
+    name: "プレイボール！～目指せ！満塁ホームラン～",
+    short: "PB",
+    bgm: "ev_bgm_02"
+  },
+  {
+    name: "シャーレの総決算with連邦生徒会",
+    short: "FSR",
+    bgm: "ev_bgm_03"
+  },
+  {
+    name: "パンデミック・ハザード～奇跡の一枚～",
+    short: "PH",
+    bgm: "ev_bgm_04"
+  },
+  {
+    name: "バレンタインの約束",
+    short: "VP",
+    bgm: "ev_bgm_05"
+  },
+  {
+    name: "特殊調査「セフィロトの樹」",
+    short: "ST",
+    bgm: "ev_bgm_06"
+  },
+  {
+    name: "Code: BOX ミレニアムに迫る影～一つの問いと二つの答え～",
+    short: "CB",
+    bgm: "ev_bgm_07"
+  },
+  {
+    name: "オペラより愛をこめて！",
+    short: "OAL",
+    bgm: "ev_bgm_08"
+  },
+  {
+    name: "陽ひらく彼女たちの小夜曲",
+    short: "SS",
+    bgm: "ev_bgm_09"
+  },
+  {
+    name: "電脳新春行進曲",
+    short: "DNM",
+    bgm: "ev_bgm_10"
+  },
+  {
+    name: "Trip-Trap-Train",
+    short: "TTT",
+    bgm: "ev_bgm_11"
+  },
+  {
+    name: "学漫同人物語～2人が求める最終回～",
+    short: "DMF",
+    bgm: "ev_bgm_12"
+  },
+  {
+    name: "隠されし遺産を求めて ～トリニティの課外活動～",
+    short: "HLT",
+    bgm: "ev_bgm_13"
+  },
+  {
+    name: "夏の特殊作戦！RABBIT小隊と消えたエビの謎",
+    short: "SSR",
+    bgm: "ev_bgm_14"
+  },
+  {
+    name: "龍武同舟～思い描くは、ひとつの未来～",
+    short: "RBF",
+    bgm: "ev_bgm_15"
+  },
+  {
+    name: "白亜の予告状～虚飾の館と美学の在り処～",
+    short: "WNB",
+    bgm: "ev_bgm_16"
+  },
+  {
+    name: "喰積の前にいささか ～一番勝負～",
+    short: "KM",
+    bgm: "ev_bgm_17"
+  },
+  {
+    name: "聖堂のメリークリスマス ～救護騎士団の贈り物～",
+    short: "SMC",
+    bgm: "ev_bgm_18"
+  },
+  {
+    name: "Get Set, Go! ～キヴォトス晄輪大祭～",
+    short: "GSG",
+    bgm: "ev_bgm_19"
+  },
+  {
+    name: "On your mark ＠ millennium ～キヴォトス晄輪大祭～",
+    short: "OYM",
+    bgm: "ev_bgm_20"
+  },
+  {
+    name: "放課後スイーツ物語 甘い秘密と銃撃戦",
+    short: "SSS",
+    bgm: "ev_bgm_21"
+  },
+  {
+    name: "出張！百夜堂海の家FC計画",
+    short: "BYH",
+    bgm: "ev_bgm_22"
+  },
+  {
+    name: "アビドスリゾート復旧対策委員会",
+    short: "ARI",
+    bgm: "ev_bgm_23"
+  },
+  {
+    name: "不忍ノ心",
+    short: "FNS",
+    bgm: "ev_bgm_24"
+  },
+  {
+    name: "どたばたシスターと古書館の魔術師",
+    short: "DSM",
+    bgm: "ev_bgm_25"
+  }
+];
